c07a470de8a11705f63b0bb7df67f5df781b15a1
Adrien <adrien.de.adrien@gmail.com>
2021-10-12 00:09:59 -0300

    change setup version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Framework to generate an html file with python'
 LONG_DESCRIPTION = 'Framework to generate an html file with python'
 
